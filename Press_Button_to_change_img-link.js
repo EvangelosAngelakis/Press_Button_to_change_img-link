e5fc9ad84e165990484b0e73386f8ccd00c09b35
Evangelos Angelakis <Evangelosangel@gmail.com>
2024-11-04 10:54:41 +0000

    Javascript final tweeks and CSS improvements
    Added code with new img and links to swich when button is pressed.
    Added improvements to CSS to keep width of images stable.

--- Press_Button_to_change_img-link.js
+++ Press_Button_to_change_img-link.js
@@ -1,3 +1,5 @@
 document.getElementById('dButton').addEventListener('click', function(){
 document.getElementById('dText').textContent = 'SeaSide Holidays';
+document.getElementById('dLink').href = 'https://www.hoseasons.co.uk/collections/beach-holidays';
+document.getElementById('dImage').src = 'https://cdn.pixabay.com/photo/2016/11/29/03/46/beach-1867137_1280.jpg';
 });
