f5440278c5d218e7525c98b43b8472ad687a0b17
Evangelos Angelakis <Evangelosangel@gmail.com>
2024-11-06 11:08:28 +0000

    Added reset button and made visual improvements.
    Added reset button that resets the page and made visual improvements.

--- Press_Button_to_change_img-link.js
+++ Press_Button_to_change_img-link.js
@@ -3,3 +3,7 @@ document.getElementById('dText').textContent = 'SeaSide Holidays';
 document.getElementById('dLink').href = 'https://www.hoseasons.co.uk/collections/beach-holidays';
 document.getElementById('dImage').src = 'https://cdn.pixabay.com/photo/2016/11/29/03/46/beach-1867137_1280.jpg';
 });
+
+function resetPage() {
+    location.reload();
+}
